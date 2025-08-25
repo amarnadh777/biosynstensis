590b501def69c73e4a86e99558742dc819f2af6f
amarnadh777 <amarnadh6565@gmail.com>
2025-08-25 14:38:41 +0530

    add

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.html"], // this tells Tailwind to scan your HTML files
+  theme: {
+    extend: {
+      fontFamily: {
+        sansation: ["Sansation", "sans-serif"], // add Sansation font
+      },
+    },
+  },
+  plugins: [],
+};
